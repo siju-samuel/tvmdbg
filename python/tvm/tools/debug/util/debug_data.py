37398d4cfd38ad60315d5597e0ffa46ee084ae63
Albin Joy <4joyalbin@gmail.com>
2018-06-19 16:02:23 +0530

    debug_utils removed

--- python/tvm/tools/debug/util/debug_data.py
+++ python/tvm/tools/debug/util/debug_data.py
@@ -130,38 +130,6 @@ def _get_tensor_watch_key(node_name, output_slot, debug_op):
     """
     return "%s:%s" % (_get_tensor_name(node_name, output_slot), debug_op)
 
-
-def has_inf_or_nan(datum, tensor):
-    """A predicate for whether a tensor consists of any bad numerical values.
-
-    This predicate is common enough to merit definition in this module.
-    Bad numerical values include `nan`s and `inf`s.
-    The signature of this function follows the requirement of the method
-    `DebugDumpDir.find()`.
-
-    Args:
-      datum: (`DebugTensorDatum`) Datum metadata.
-      tensor: (`numpy.ndarray` or None) Value of the tensor. None represents
-        an uninitialized tensor.
-
-    Returns:
-      (`bool`) True if and only if tensor consists of any nan or inf values.
-    """
-
-    _ = datum  # Datum metadata is unused in this predicate.
-
-    if isinstance(tensor, InconvertibleTensorProto):
-        # Uninitialized tensor doesn't have bad numerical values.
-        # Also return False for data types that cannot be represented as numpy
-        # arrays.
-        return False
-    elif (np.issubdtype(tensor.dtype, np.floating) or
-          np.issubdtype(tensor.dtype, np.complex) or
-          np.issubdtype(tensor.dtype, np.integer)):
-        return np.any(np.isnan(tensor)) or np.any(np.isinf(tensor))
-    return False
-
-
 _CoreMetadata = collections.namedtuple("CoreMetadata", [
     "global_step", "session_run_index", "executor_step_index", "input_names",
     "output_names", "target_nodes"
